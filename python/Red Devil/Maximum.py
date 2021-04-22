ff31bc76d90bce5821141a0f651753bf56161a36
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-22 23:45:31 +0530

    Create Maximum.py

---
+++ python/Red Devil/Maximum.py
@@ -0,0 +1,9 @@
+"""
+Programme to find maximum of two numbers
+"""
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+if (a>b):
+    print(a,"is bigger number")
+else:
+    print(b,"is bigger number")
