4f8a662c7a1d29468162f57e0e33473264f264e8
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-22 23:58:20 +0530

    Update showNumbers.py

--- python/Red Devil/showNumbers.py
+++ python/Red Devil/showNumbers.py
@@ -1,4 +1,6 @@
-"""Write a function called showNumbers that takes a parameter called limit. It should print all the numbers between 0 and limit with a label to identify the even and odd numbers. For example, if the limit is 3, it should print:
+"""
+Write a function called showNumbers that takes a parameter called limit. It should print all the numbers between 0 and limit with a label to identify the even and odd numbers.
+For example, if the limit is 3, it should print:
 0 EVEN
 1 ODD
 2 EVEN
