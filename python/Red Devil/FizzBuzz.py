f2a1a2a3a4c07408caead42a0dffcb216b2e728f
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-22 23:46:09 +0530

    Create FizzBuzz.py

---
+++ python/Red Devil/FizzBuzz.py
@@ -0,0 +1,18 @@
+"""
+    Write a function called fizz_buzz that takes a number.
+1.  If the number is divisible by 3, it should return “Fizz”.
+2.  If it is divisible by 5, it should return “Buzz”.
+3.  If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+4.  Otherwise, it should return the same number."""
+
+
+def fizz_buzz(n):
+    if (n % 3 == 0 and n % 5 == 0):
+        return("FizzBuzz")
+    if(n%3==0):
+        return("Fizz")
+    if(n%5==0):
+        return("Buzz")
+    return(n)
+if __name__ == "__main__":
+    print(fizz_buzz(int(input("Enter the number "))))
