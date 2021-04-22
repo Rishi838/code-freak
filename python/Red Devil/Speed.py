2291f930e3afd3e91f82e7ba64ff1bacdfa0a20a
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-22 23:48:44 +0530

    Create Speed.py

---
+++ python/Red Devil/Speed.py
@@ -0,0 +1,18 @@
+"""Write a function for checking the speed of drivers. This function should have one parameter: speed.
+1.  If speed is less than 70, it should print “Ok”.
+2.  Otherwise, for every 5km above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+    For example, if the speed is 80, it should print: “Points: 2”.
+3.  If the driver gets more than 12 points, the function should print: “License suspended"
+"""
+
+
+def check_speed(speed):
+    if (speed <= 70):
+        print("OK")
+    else:
+        points = (speed - 70) // 5
+        if (points > 12):
+            print("License Suspended")
+        else:
+            print("Points :", points)
+check_speed(int(input("Enter speed ")))
