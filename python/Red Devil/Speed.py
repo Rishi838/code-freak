5ff99020b5583aac0264a54f1783b114b6600048
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-22 23:58:54 +0530

    Update Speed.py

--- python/Red Devil/Speed.py
+++ python/Red Devil/Speed.py
@@ -1,4 +1,5 @@
-"""Write a function for checking the speed of drivers. This function should have one parameter: speed.
+"""
+Write a function for checking the speed of drivers. This function should have one parameter: speed.
 1.  If speed is less than 70, it should print “Ok”.
 2.  Otherwise, for every 5km above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
     For example, if the speed is 80, it should print: “Points: 2”.
