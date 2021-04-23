eef825f370553ed46d92a6f6895a1cd164ef7ac1
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-23 10:53:36 +0530

    Create Iterations.py

---
+++ python/Red Devil/Iterations.py
@@ -0,0 +1,8 @@
+"""
+Given a range of the first 10 numbers, Iterate from the start number to the end number,
+and In each iteration print the sum of the current number and previous number
+"""
+a = 0
+for i in range(10):
+    print(a+i)
+    a = i
