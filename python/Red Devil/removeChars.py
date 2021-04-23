ddbca0547ebdc78c7e067b787522c706e1b2ae93
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-23 10:57:55 +0530

    Create removeChars.py

---
+++ python/Red Devil/removeChars.py
@@ -0,0 +1,6 @@
+"""
+Given a string and an integer number n, remove characters from a string starting from zero up to n and return a new string
+"""
+s = input("Enter the String ")
+n = int(input("Enter number "))
+print(s[n:])
