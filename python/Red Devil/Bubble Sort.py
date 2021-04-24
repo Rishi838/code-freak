e84a7df958b0bdd0a211036576081f5aba6802fe
RedDevil0906 <80878197+RedDevil0906@users.noreply.github.com>
2021-04-24 19:11:24 +0530

    Create Bubble Sort.py

---
+++ python/Red Devil/Bubble Sort.py
@@ -0,0 +1,10 @@
+lst = list(map(str, input("Enter the list elements seperated by space").split())) #To input a string array
+for i in range(len(lst)):
+    for j in range(len(lst)-i-1):
+        if (len(lst[j]) > len(lst[j + 1])): #If length of element at j is bigger than j+1 then interchange them
+            lst[j], lst[j + 1] = lst[j + 1], lst[j] #To interchange elements of array at index j and j+1
+        elif (len(lst[j]) == len(lst[j + 1])):  #If length of element at j is equal to j+1 then run a loop from 0 to their legth
+            for k in range(len(lst[j])):  
+                if (lst[j][k] > lst[j + 1][k]): #Check each element of both strings and if string at l is bigger than string at l+1
+                    lst[j], lst[j + 1] = lst[j + 1], lst[j] #To interchange elements of array at index j and j+1
+print(lst)
