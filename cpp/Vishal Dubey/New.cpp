ce8e65f3b9c6315868a74f2c03287b77fa49249b
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-05-17 19:49:35 +0530

    Create New.cpp

---
+++ cpp/Vishal Dubey/New.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+    int x;
+    cin>>x;
+    int a;
+  cin>>a;
+  int b;
+  cin>>b;
+  int j;
+   j=a*10+1000*b-x*b*10;
+  cout<<j<<endl;
+    }}
