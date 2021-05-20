0bc8eea040a13eed4c3c36bbf8e518b1cc919cbf
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-05-20 22:39:24 +0530

    Create  And Then There Were K.cpp

---
+++ cpp/Vishal Dubey/And Then There Were K.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin>>t;
+  while(t--){
+     long long int n,k,p;
+      cin>>n;
+       k=log2(n);
+       {
+           p=pow(2,k)-1;
+       }
+
+      cout<<p<<endl;
+  }}
