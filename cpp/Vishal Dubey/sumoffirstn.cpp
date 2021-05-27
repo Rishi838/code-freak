84c3aea56e87d977952120ebdbb1144522a9f435
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-05-27 09:40:30 +0530

    Create sumoffirstn.cpp

---
+++ cpp/Vishal Dubey/sumoffirstn.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+    sum=sum+i;
+    }
+    printf("sum of numbers %d",sum);
+}
