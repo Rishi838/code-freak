91445b62dcc3d1a29582f73e0c607bd65eb76a92
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-06-01 00:08:01 +0530

    Create hoops.cpp

---
+++ cpp/Vishal Dubey/hoops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+       int n,k,p;
+       cin>>n;
+       k=(n-1)/2;
+       p=k+1;
+       cout<<p<<endl;
+    }
+    }
