f285236b9f0506a6879c10842661cc5203f8223a
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-06-09 16:32:16 +0530

    Create ctof.c

---
+++ cpp/Vishal Dubey/ctof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    printf("Enter the tempreatur in celecius:");
+    int n;
+    scanf("%d",&n);
+    float k=(9*n/5)+32;
+    printf("\nCelcius to farhenite:%f",k);
+}
