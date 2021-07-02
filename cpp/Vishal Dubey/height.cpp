73c780b98e646a95319136646b8d655003716ebc
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-07-02 21:13:42 +0530

    Create height.cpp

---
+++ cpp/Vishal Dubey/height.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--){
+      ll g,c;
+        cin>>g>>c;
+        ll k,j,l;
+        k=c*c;
+        j=2*g;
+        l=k/j;
+        cout<<l<<endl;
+        
+    }
+}
