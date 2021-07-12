cbc672c2c1136bb63b4d8598c56f0aa6bfa391e2
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-07-12 23:14:58 +0530

    Create union basic.cpp

---
+++ cpp/Vishal Dubey/union basic.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+using namespace std;
+
+int main() {
+union vishal{
+    int age;
+    char gender;
+    
+} ;
+
+union vishal a;
+a.age=34;
+a.gender='m';
+cout<<a.age<<endl<<a.gender<<endl;
+}
